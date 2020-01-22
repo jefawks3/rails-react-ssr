d6a2881ff8f4d8ce9ae16798ff9dcbe9f3756ea4
James Fawks <jefawks3@gmail.com>
2020-01-22 12:22:30 -0500

    Adding a test to make sure the bundle url is being generated correctly for the webpacker-dev-server

--- test/webpacker_utils_test.rb
+++ test/webpacker_utils_test.rb
@@ -13,7 +13,7 @@ class RailsReactSSR::WebpackerUtilsTest < RailsReactSSR::Test
 
   def test_bundle_found!
     assert_equal RailsReactSSR::WebpackerUtils.hashed_bundle_name!('application.js'),
-                 "/packs/application-k344a6d59eef8632c9d1.js"
+                 '/packs/application-k344a6d59eef8632c9d1.js'
   end
 
   def test_open_local_file
@@ -27,6 +27,15 @@ class RailsReactSSR::WebpackerUtilsTest < RailsReactSSR::Test
   def test_open_remote_file
     # TODO Run dev server during tests to make sure remote file is accessible
     skip 'Need to find a way to run the dev server during the tests'
+  end
+
+  def test_build_remote_uri
+    with_rails_env 'development' do
+      hashed_bundle = '/packs/application-k344a6d59eef8632c9d1.js'
+      uri = RailsReactSSR::WebpackerUtils.send :dev_bundle_uri, hashed_bundle
+
+      assert_equal uri, 'http://localhost:3035/packs/application-k344a6d59eef8632c9d1.js'
+    end
   end
 
   private
